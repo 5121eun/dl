aec9dc57a43b4b03262f814f9fcf3cf16c0223c0
5121eun <121006954+5121eun@users.noreply.github.com>
2023-03-01 23:43:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='models',
+    packages = ['models']
     version='0.0.3',
     url='https://github.com/5121eun/models.git',
     install_requires=[
