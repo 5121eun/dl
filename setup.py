5681ea7fd2d2f8dd8e5aa0bd98d7cc6e61f28be1
5121eun <121006954+5121eun@users.noreply.github.com>
2023-03-01 23:30:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from setuptools import setup
-
-
-# In[6]:
-
 
 setup(
     name='models',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/5121eun/models.git',
     install_requires=[
         'torch'
     ]
 )
-
-
-# In[ ]:
-
-
-
-
