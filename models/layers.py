41965ed253477a91472e94922783a4d8e53d6bba
5121eun <121006954+5121eun@users.noreply.github.com>
2023-03-07 15:49:56 +0900

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -16,7 +16,7 @@ class FeedForward(nn.Sequential):
         )
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, dim: int, nheads: int, dropout = 0.):
+    def __init__(self, dim: int, nheads: int, dropout = 0., final_ln=False):
         super(MultiHeadAttention, self).__init__()
         
         assert dim % nheads == 0, 'dim % nheads != 0'
@@ -26,15 +26,18 @@ class MultiHeadAttention(nn.Module):
         self.v = nn.Linear(dim, dim, bias=False)
         
         self.dropout = nn.Dropout(dropout)
-
+        if final_ln:
+            self.ln = nn.LayerNorm(dim // nheads)
+        
         self.out = nn.Sequential(
             nn.Linear(dim, dim, bias=False),
             nn.Dropout(dropout)
         )
         
         self.nheads = nheads
+        self.final_ln = final_ln
         
-    def forward(self, x, mask=None):
+    def forward(self, x, seq_mask=None, sparse_masks=None, is_final=False):
         h = self.nheads
         b, n, d = x.shape
         dk = d // h
@@ -48,22 +51,29 @@ class MultiHeadAttention(nn.Module):
         
         attn = torch.matmul(q, k.transpose(2, 3)) / (dk ** 0.5)
         
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
+        if seq_mask is not None:
+            attn = attn.masked_fill(seq_mask == 0, -1e9)
+        
+        if sparse_masks is not None:
+            attn[:, 0::2] = attn[:, 0::2].masked_fill(sparse_masks[0] == 0, -1e9)
+            attn[:, 1::2] = attn[:, 1::2].masked_fill(sparse_masks[1] == 0, -1e9)
         
         attn = self.dropout(F.softmax(attn, dim=-1))
         attn = torch.matmul(attn, v)
         
+        if self.final_ln & is_final:
+            attn = self.ln(attn)
+        
         out = attn.transpose(1, 2).contiguous().view(b, n, d)
         out = self.out(out)
         return out
-
+    
 class TransformerBlock(nn.Module):
-    def __init__(self, dim: int, nheads: int, hid_dim: int, dropout = 0.):
+    def __init__(self, dim: int, nheads: int, hid_dim: int, dropout = 0., **kwargs):
         super(TransformerBlock, self).__init__()
         
         self.ln1 = nn.LayerNorm(dim)
-        self.attn = MultiHeadAttention(dim, nheads, dropout)
+        self.attn = MultiHeadAttention(dim, nheads, dropout, **kwargs)
         
         self.ln2 = nn.LayerNorm(dim)
         self.ff = FeedForward(dim, hid_dim, nn.GELU(), dropout)
