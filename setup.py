19c8a87c7e76c1e821fad9f2aeebc8fbb74c9375
5121eun <121006954+5121eun@users.noreply.github.com>
2023-03-09 01:06:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='models',
     packages = ['models'],
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/5121eun/models.git',
     install_requires=[
         'torch'
