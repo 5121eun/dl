3ea670cd7a6ae208ac785304f6eb1d43a7b2680b
5121eun <121006954+5121eun@users.noreply.github.com>
2023-08-24 04:46:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dl',
-    packages = ['models'],
+    packages = find_packages(exclude=[]),
     version='0.1.0',
     url='https://github.com/5121eun/dl.git',
     install_requires=[
