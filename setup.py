83ca3599086ad27da4ce9b099ffd9788c9e300c6
5121eun <121006954+5121eun@users.noreply.github.com>
2023-08-24 04:55:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='dl',
-    packages = find_packages(exclude=[]),
-    version='0.1.2',
+    packages = find_packages(),
+    version='0.1.3',
     url='https://github.com/5121eun/dl.git',
     install_requires=[
         'torch'
